3167ad2c061dd9511950e65d02c2cdeb3df42e6a
mnseong <mskweon@icloud.com>
2023-02-25 16:01:21 +0900

    Implement multi head attention class

--- transformer-architecture/encoder/attention_head.py
+++ transformer-architecture/encoder/attention_head.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn
 from scaled_dot_product_attention import scaled_dot_product_attention
 
@@ -13,3 +14,20 @@ class AttentionHead(nn.Module):
         attn_outputs = scaled_dot_product_attention(
             self.q(hidden_state), self.k(hidden_state), self.v(hidden_state))
         return attn_outputs
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        embed_dim = config.hidden_size
+        num_heads = config.num_attention_heads
+        head_dim = embed_dim // num_heads
+        self.heads = nn.ModuleList(
+            [AttentionHead(embed_dim, head_dim) for _ in range(num_heads)]
+        )
+        self.output_linear = nn.Linear(embed_dim, embed_dim)
+
+    def forward(self, hidden_state):
+        x = torch.cat([h(hidden_state) for h in self.heads], dim=-1)
+        x = self.output_linear(x)
+        return x
