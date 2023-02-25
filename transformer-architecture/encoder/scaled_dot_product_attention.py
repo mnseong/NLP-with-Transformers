f73a4fa751338609fa24a33a9052212f704aee20
mnseong <mskweon@icloud.com>
2023-02-25 15:56:08 +0900

    Implement scaled dot product attention class

---
+++ transformer-architecture/encoder/scaled_dot_product_attention.py
@@ -0,0 +1,7 @@
+import torch
+
+def scaled_dot_product_attention(query, key, value):
+    dim_k = query.size(-1)
+    scores = torch.bmm(query, key.transpose(1,2))/sqrt(dim_k)
+    weights = F.softmax(scores, dim=1)
+    return torch.bmm(weights, value)
