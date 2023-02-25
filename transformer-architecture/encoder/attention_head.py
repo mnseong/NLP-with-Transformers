bcbef2f0e94effe2ce73cf8ccc9412d0ea790b90
mnseong <mskweon@icloud.com>
2023-02-25 15:56:18 +0900

    Implement attention head class

---
+++ transformer-architecture/encoder/attention_head.py
@@ -0,0 +1,15 @@
+from torch import nn
+from scaled_dot_product_attention import scaled_dot_product_attention
+
+
+class AttentionHead(nn.Module):
+    def __init__(self, embed_dim, head_dim):
+        super().__init__()
+        self.q = nn.Linear(embed_dim, head_dim)
+        self.k = nn.Linear(embed_dim, head_dim)
+        self.v = nn.Linear(embed_dim, head_dim)
+
+    def forward(self, hidden_state):
+        attn_outputs = scaled_dot_product_attention(
+            self.q(hidden_state), self.k(hidden_state), self.v(hidden_state))
+        return attn_outputs
