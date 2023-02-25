9691dcb0ab4b6f0abf54ad22597cb3aa8b1745ad
mnseong <mskweon@icloud.com>
2023-02-25 17:35:18 +0900

    Implement Encoder extend version for sequence classifier

--- transformer-architecture/encoder/transformer_encoder.py
+++ transformer-architecture/encoder/transformer_encoder.py
@@ -12,3 +12,17 @@ class TransformerEncoder(nn.Module):
         for layer in self.layers:
             x = layer(x)
         return x
+
+
+class TransformerForSequenceClassification(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.encoder = TransformerEncoder(config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.classifier = nn.Linear(config.hidden_size, config.num_labels)
+
+    def forward(self, x):
+        x = self.encoder(x)[:, 0, :]
+        x = self.dropout(x)
+        x = self.classifier(x)
+        return x
