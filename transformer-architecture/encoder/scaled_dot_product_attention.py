5eaf13c3b90a2a9dcbc8cb42784535491f57c231
mnseong <mskweon@icloud.com>
2023-02-28 20:04:15 +0900

    Add masking operation

--- transformer-architecture/encoder/scaled_dot_product_attention.py
+++ transformer-architecture/encoder/scaled_dot_product_attention.py
@@ -1,7 +1,12 @@
 import torch
+import torch.nn.functional as F
+from math import sqrt
 
-def scaled_dot_product_attention(query, key, value):
+def scaled_dot_product_attention(query, key, value, mask=None):
     dim_k = query.size(-1)
     scores = torch.bmm(query, key.transpose(1,2))/sqrt(dim_k)
-    weights = F.softmax(scores, dim=1)
+    # masking step
+    if mask is not None:
+        scores = scores.masked_fill(mask==0, float("-inf"))
+    weights = F.softmax(scores, dim=-1)
     return torch.bmm(weights, value)
